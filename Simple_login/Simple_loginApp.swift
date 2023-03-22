87c5499a3c93fb5b1e730c7f47af70c7c4bb8403
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2023-03-21 19:40:19 -0500

    Initial Commit

---
+++ Simple_login/Simple_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_loginApp.swift
+//  Simple_login
+//
+//  Created by Nicole Barnhouse on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
